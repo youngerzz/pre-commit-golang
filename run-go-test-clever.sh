220de96f3a3ee319b150193829e9f63ad6338219
Yang Zhao <zhaoyang@caicloud.io>
2018-01-31 16:39:47 +0800

    my test

--- run-go-test-clever.sh
+++ run-go-test-clever.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-cd /Users/younger/projects/dev/go/src/github.com/caicloud/kubeflow-admin/
-make test-local
+# cd /Users/younger/projects/dev/go/src/github.com/caicloud/kubeflow-admin/
+# make test-local
+echo "test my pre-commit"
